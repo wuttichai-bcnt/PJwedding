0e3b1a3b9442039f183a18005c411bcd29131403
Wuttichai Phetthong <87232563+wuttichai-bcnt@users.noreply.github.com>
2022-06-08 22:32:31 +0700

    insert Data

---
+++ save.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$fp=fopen("datacollection.csv","a+"); //use a+ instead of w
+fputcsv($fp, array($name, (string)$email), ',');
+fclose($fp);
+
+header( "location: http://localhost/PJwedding" );
+
+?>
