7c4a062e2caff29eac89a6979a482f931b7714f6
Wuttichai Phetthong <87232563+wuttichai-bcnt@users.noreply.github.com>
2022-06-14 09:16:21 +0700

    send data to Google Sheet

---
+++ fn.js
@@ -0,0 +1,35 @@
+function testGS(){
+    const url = "https://script.google.com/macros/s/AKfycbzgy9CoZ3fCArJD6A4K3zYpbxVJfBFNVhOeLQiB_db2lzOgnbY/exec"
+
+    fetch(url)
+        .then(d => d.json())
+        .then(d => {
+            document.getElementById("app").textContent = d[0].status;
+        });
+}
+
+function addGS(){
+    const url = "https://script.google.com/macros/s/AKfycbzgy9CoZ3fCArJD6A4K3zYpbxVJfBFNVhOeLQiB_db2lzOgnbY/exec"
+
+    var wish = document.getElementById("wish").value;
+    var name = document.getElementById("name").value;
+    var phoneNo = document.getElementById("phoneNo").value;
+    var address = document.getElementById("address").value;
+
+
+      fetch(url, {
+          method: "POST",
+          mode: 'no-cors',
+          body: JSON.stringify({"col1": wish, "col2": name, "col3": phoneNo,"col4":address}),
+          headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json()) 
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+      
+        console.log("Clicked1");
+       
+}
+
+// document.getElementById("btn2").addEventListener("click",testGS)
+document.getElementById("btn").addEventListener("click",addGS)
